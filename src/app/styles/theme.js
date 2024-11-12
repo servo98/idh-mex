2ecdf929d29db1eae83ee52784ed1ef9b97f3d99
servo pc <fernandonando98@live.com.mx>
2024-11-12 15:24:09 -0600

    fix: make theme font preloaded false

--- src/app/styles/theme.js
+++ src/app/styles/theme.js
@@ -2,7 +2,11 @@
 import { createTheme } from "@mui/material";
 import { Roboto } from "next/font/google";
 
-const roboto = Roboto({ weight: ["300", "400", "500", "700"] });
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  preload: false,
+});
 
 //TODO: crear tema on MUI theme generator
 const theme = createTheme({
